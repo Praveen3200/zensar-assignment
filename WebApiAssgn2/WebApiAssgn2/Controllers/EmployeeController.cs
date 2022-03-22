2f38abb38af20180c6f406a3e24b06df915e0cb7
Praveen3200 <98096075+Praveen3200@users.noreply.github.com>
2022-03-22 15:38:21 +0530

    WebAPIAssignment2

---
+++ WebApiAssgn2/WebApiAssgn2/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiAssgn2.Models;
+
+namespace WebApiAssgn2.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        zensarDbEntities zdb = new zensarDbEntities();
+        public IHttpActionResult GetEmployeeSalById(int eid)
+        {
+            var details = zdb.getEmpsalary(eid).ToList();
+            return Ok(details);
+        }
+    }
+}
---
+++ WebApiAssgn2/WebApiAssgn2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApiAssgn2.WebApiApplication" Language="C#" %>
